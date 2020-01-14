76f7b7649221d1bf548e44875b7012d3b505800c
Debs <ddurojaiye@gmail.com>
2020-01-14 18:35:38 +0000

    Add routes for gas and other emergency list items

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,20 @@ router.post("/prior-repair-answer", function(req, res) {
     }
   });
 
+
+// Routes for emergency repairs
+
+router.post("/priority-repair-answer", function(req, res) {
+    if (req.session.data["repair-emergency"] === "none-of-these") {
+      res.redirect("/repair-location");
+    } else if (req.session.data["repair-emergency"] === "gas")  {
+      res.redirect("/smell-gas");
+    } else {
+        res.redirect("/emergency-repair");
+
+    }
+  });
+
 // Routes for different repair descriptions
 
 router.post("/repair-location-kitchen-answer", function(req, res) {
