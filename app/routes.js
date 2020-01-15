fad8875333aa8b0a8e02f57339214d6a70ebcc14
Debs <ddurojaiye@gmail.com>
2020-01-15 14:22:56 +0000

    Add leaks repair journey

--- app/routes.js
+++ app/routes.js
@@ -28,9 +28,11 @@ router.post("/priority-repair-answer", function(req, res) {
 
 // Routes for different repair descriptions
 
-router.post("/repair-location-kitchen-answer", function(req, res) {
-    if (req.session.data["repair-location-kitchen"] === "Damp") {
-      res.redirect("/repair-damp");
+router.post("/repair-description-answer", function(req, res) {
+    if (req.session.data["repair-location-kitchen"] === "Damp or mould") {
+      res.redirect("/repair-description-damp");
+    } else if (req.session.data["repair-location-kitchen"] === "Drip") {
+      res.redirect("/repair-description-leak");
     } else {
       res.redirect("/repair-description");
     }
@@ -41,6 +43,34 @@ router.post("/repair-location-kitchen-answer", function(req, res) {
       res.redirect("/repair-damp");
     } else if (req.session.data["repair-description"] === "Mold"){
       res.redirect("/repair-description-damp-mold");
+    } else {
+      res.redirect("/repair-description")
+    }
+  });
+
+  router.post("/repair-description-leak-answer", function(req, res) {
+    if (req.session.data["repair-leak"] === "dripping-from-wall") {
+      res.redirect("/repair-description-leak-electrics");
+    } else if (req.session.data["repair-leak"] === "tap-broken"){
+      res.redirect("/emergency-repair");
+    } else {
+      res.redirect("/repair-description")
+    }
+  });
+
+  router.post("/repair-description-leak-electrics-answer", function(req, res) {
+    if (req.session.data["repair-leak-electrics"] === "dripping-on-electrics") {
+      res.redirect("/emergency-repair");
+    } else if (req.session.data["repair-leak-electrics"] ===  "not-containable"){
+      res.redirect("/emergency-repair");
+    } else {
+      res.redirect("/repair-description-leak-inside")
+    }
+  });
+
+  router.post("/repair-description-leak-source-answer", function(req, res) {
+    if (req.session.data["repair-leak-source"] === "condensation") {
+      res.redirect("/repair-damp");
     } else {
       res.redirect("/repair-description")
     }
